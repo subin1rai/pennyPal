a8b3711a119136a22a5621d789f9b2d53ef9c8b5
subin1rai <bantawasubin@gmail.com>
2025-01-03 23:39:44 +0545

    debts features added

--- obj/Debug/net9.0-windows10.0.19041.0/win10-x64/PennyPal.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows10.0.19041.0/win10-x64/PennyPal.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PennyPal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4e8394492abe1b959badc232a30e54e7ec49a778")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+86189241a4c0546001a01fe057e309a0538fa28c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PennyPal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PennyPal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
