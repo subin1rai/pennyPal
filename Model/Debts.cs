a5ed7b470966968bd28d0e3747164482f48a2036
subin1rai <bantawasubin@gmail.com>
2025-01-04 20:46:54 +0545

    dashboard complete

--- Model/Debts.cs
+++ Model/Debts.cs
@@ -6,13 +6,23 @@ using System.Threading.Tasks;
 
 namespace PennyPal.Model
 {
+    public enum DebtStatus
+    {
+        Pending,
+        Paid,
+        Overdue,
+        Clear // Added Clear status
+    }
+
+
     public class Debts
     {
         public int Id { get; set; }
-       
         public decimal Amount { get; set; } // Total debt amount
         public decimal PaidAmount { get; set; } // Amount paid towards debt
         public DateTime Date { get; set; }
         public string Description { get; set; }
+        public DebtStatus Status { get; set; } // Enum to represent the status
     }
+
 }
